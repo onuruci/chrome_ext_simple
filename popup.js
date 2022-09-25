4b79283ffc2770d040cd9db1c917341f050fe444
onuruci <onur.yuce@metu.edu.tr>
2022-09-25 17:12:39 +0300

    new style and functionality

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 
 
-let changeColor = document.getElementById("changeImageButton");
+let changeColor = document.getElementById("giveMeme");
 
 chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+  
 });
 
 // When the button is clicked, inject setPageBackgroundColor into current page
@@ -20,11 +20,36 @@ changeColor.addEventListener("click", async () => {
 // current page
 function setPageBackgroundColor() {
   chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-    let items = document.getElementsByTagName("img");
-    alert(items);
-    for(let i = 0 ; i < items.length ; i++){
-      items[i].src = chrome.runtime.getURL('m.jpg');
-    }
+
+    let dv = document.createElement('div');
+    dv.id = "divvclass";
+    dv.style.backgroundImage = 'url('+chrome.runtime.getURL('bg.png')+')';
+    //dv.style.backgroundColor = "#b65353";
+
+    let imgcont = document.createElement('div');
+    imgcont.id = "imgcont";
+    let divv = document.createElement('img');
+    divv.className = "imgclass";
+    divv.src = chrome.runtime.getURL('Memes/'+(Math.floor(Math.random() * 18) + 1).toString()+'.jpg');
+    imgcont.appendChild(divv);
+    dv.appendChild(imgcont);
+    document.body.appendChild(dv);
+
+    dv.onClick = () => {
+      console.log("Hellooo");
+    };
+    document.addEventListener('click', function() {
+      var link = document.getElementById('divvclass');
+      // onClick's logic below:
+      if(link){
+          document.body.removeChild(link);
+      }
+      
+  });
+
+    // let items = document.getElementsByTagName("img");
+    // for(let i = 0 ; i < items.length ; i++){
+    //   items[i].src = chrome.runtime.getURL('m.jpg');
+    // }
   });
 }
