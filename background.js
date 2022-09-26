357f65339eadcf7ac6a717e1a57a57ea97828fc6
onuruci <onur.yuce@metu.edu.tr>
2022-09-26 21:07:54 +0300

    added some neccessary things

--- background.js
+++ background.js
@@ -2,7 +2,8 @@
 
 let color = '#3aa757';
 
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(async () => {
+  let [color] = await chrome.tabs.query({ active: true, currentWindow: true });
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
--- popup.js
+++ popup.js
@@ -6,24 +6,17 @@ chrome.storage.sync.get("color", ({ color }) => {
   
 });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    func: setPageBackgroundColor,
-  });
-});
-
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
+function setPageBackgroundColor(tab) {
   chrome.storage.sync.get("color", ({ color }) => {
+
+    let maindivv = document.createElement('div');
+    maindivv.id = "maindivv";
 
     let dv = document.createElement('div');
     dv.id = "divvclass";
     dv.style.backgroundImage = 'url('+chrome.runtime.getURL('bg.png')+')';
+    
     //dv.style.backgroundColor = "#b65353";
 
     let imgcont = document.createElement('div');
@@ -31,15 +24,14 @@ function setPageBackgroundColor() {
     let divv = document.createElement('img');
     divv.className = "imgclass";
     divv.src = chrome.runtime.getURL('Memes/'+(Math.floor(Math.random() * 18) + 1).toString()+'.jpg');
+    dv.style.top = (window.pageYOffset + 100).toString() + 'px';
     imgcont.appendChild(divv);
     dv.appendChild(imgcont);
-    document.body.appendChild(dv);
+    maindivv.appendChild(dv);
+    document.body.appendChild(maindivv);
 
-    dv.onClick = () => {
-      console.log("Hellooo");
-    };
     document.addEventListener('click', function() {
-      var link = document.getElementById('divvclass');
+      var link = document.getElementById('maindivv');
       // onClick's logic below:
       if(link){
           document.body.removeChild(link);
@@ -53,3 +45,17 @@ function setPageBackgroundColor() {
     // }
   });
 }
+
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: setPageBackgroundColor
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
